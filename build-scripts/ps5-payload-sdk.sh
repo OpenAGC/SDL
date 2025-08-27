8750ac8e3d5af3640045d740d23a604e7f12ff40
John Törnblom <john.tornblom@gmail.com>
2025-08-27 20:30:04 +0200

    ps5: add build script

---
+++ build-scripts/ps5-payload-sdk.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+SCRIPT_PATH="${BASH_SOURCE[0]}"
+SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
+
+
+if [ -z "${PS5_PAYLOAD_SDK}" ]; then
+    echo "PS5_PAYLOAD_SDK is undefined"
+    exit 1
+fi
+
+source "${PS5_PAYLOAD_SDK}/toolchain/prospero.sh"
+
+${CMAKE} -DCMAKE_BUILD_TYPE=Release \
+	 -DSDL_OPENGL=YES \
+	 -DSDL_LOADSO=YES \
+	 -B build-ps5 \
+         -S "${SCRIPT_DIR}/.."
+${MAKE} -C build-ps5
+
+
+${CMAKE} -DCMAKE_BUILD_TYPE=Release \
+	 -DSDL_OPENGL=YES \
+	 -DSDL_LOADSO=YES \
+	 -B test-ps5 \
+         -S "${SCRIPT_DIR}/../test"
+${MAKE} -C test-ps5
